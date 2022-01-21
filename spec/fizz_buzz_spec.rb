1cf113e16200fed7b6604dd0f12bb519ebbea0df
Thomas <thomas@craftacademy.se>
2022-01-21 11:10:33 +0100

    adds specs for buzz and fizz-buzz rule

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe FizzBuzz do
     result = subject.check(3)
     expect(result).to eq 'fizz'
   end
+
+  it "is expected to return 'buzz' if the number is divisable by 5" do
+    result = subject.check(5)
+    expect(result).to eq 'buzz'
+  end
+
+  it "is expected to return 'fizz-buzz' if the number is divisable by 15" do
+    result = subject.check(15)
+    expect(result).to eq 'fizz-buzz'
+  end
 end
--- src/fizz_buzz.rb
+++ src/fizz_buzz.rb
@@ -1,7 +1,11 @@
 class FizzBuzz
   def check(number)
-    if number % 3 == 0 # % is MODULUS operator
+    if number % 15 == 0
+      return 'fizz-buzz'
+    elsif number % 3 == 0 # % is MODULUS operator
       return 'fizz'
+    elsif number % 5 == 0
+      return 'buzz'
     else
      return number
     end
